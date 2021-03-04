48f39e41d0bd10a4782610212544ae94e7abbce8
93hamza <hamzeh.ajami@gmail.com>
2021-03-04 15:18:36 +0200

    lab4

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log ("hi java");
+console.log (101);
+
+alert ("hi");
+
+var grade = prompt("please enter you grade");
+if (grade>60){
+    alert("you pass");
+}
+else if  (grade<60){
+    alert("you failed");
+
+}
+
+var a = 1;
+var b = 2;
+console.log (a>b);
+console. log(a<b);
+console. log (a==b);
